76549e2faeb0eb661e6c3695a96276f7a34c3b3b
Ibragimov Eldar <90416969+ibragimoov@users.noreply.github.com>
2022-08-19 22:09:19 +0300

    update post

--- src/pages/AddPost/index.jsx
+++ src/pages/AddPost/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { useNavigate, Navigate } from "react-router-dom";
+import { useNavigate, Navigate, useParams } from "react-router-dom";
 
 import axios from "../../axios";
 
@@ -16,8 +16,11 @@ import { selectIsAuth } from "../../redux/slices/auth";
 import { useRef } from "react";
 
 export const AddPost = () => {
+    const { id } = useParams();
     const isAuth = useSelector(selectIsAuth);
     const navigate = useNavigate();
+
+    const isEditing = Boolean(id);
 
     const [isLoading, setIsLoading] = React.useState(false);
     const [value, setValue] = React.useState("");
@@ -58,11 +61,13 @@ export const AddPost = () => {
                 tags: tagsArray,
             };
 
-            const { data } = await axios.post("/posts", fields);
+            const { data } = isEditing
+                ? await axios.patch(`/posts/${id}`, fields)
+                : await axios.post("/posts", fields);
 
-            const id = data._id;
+            const _id = isEditing ? id : data._id;
 
-            navigate(`/posts/${id}`);
+            navigate(`/posts/${_id}`);
         } catch (error) {
             console.log(error);
             alert("Ошибка при создании статьи");
@@ -83,6 +88,15 @@ export const AddPost = () => {
         }),
         []
     );
+
+    React.useEffect(() => {
+        axios.get(`/posts/${id}`).then(({ data }) => {
+            setTitle(data.title);
+            setValue(data.text);
+            setImageUrl(data.imageUrl);
+            setTags(data.tags);
+        });
+    }, []);
 
     if (!window.localStorage.getItem("token") && !isAuth) {
         return <Navigate to="/" />;
@@ -145,7 +159,7 @@ export const AddPost = () => {
             />
             <div className={styles.buttons}>
                 <Button onClick={onSubmit} size="large" variant="contained">
-                    Опубликовать
+                    {isEditing ? "Сохранить" : "Опубликовать"}
                 </Button>
                 <a href="/">
                     <Button size="large">Отмена</Button>
